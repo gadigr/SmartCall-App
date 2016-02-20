527257939a45b3f4d3cba5b5e82d75b5ff8c46f9
Gadi Gomez <gadigr@gmail.com>
2016-02-20 16:47:37 +0200

    commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', 'ionic-material', 'clientsService', 'ngStorage']);
+var app = angular.module('starter', ['ionic', 'ionic-material', 'clientsService', 'ngStorage', 'ionic-datepicker', 'ionic-timepicker']);
 
 app.run(function ($ionicPlatform, $ionicPopup) {
     $ionicPlatform.ready(function () {
--- www/js/controllers/publicCtrl.js
+++ www/js/controllers/publicCtrl.js
@@ -2,7 +2,29 @@
 
   ionicMaterialInk.displayEffect();
 
-  $scope.images = [];
+  $scope.datepickerObject = {
+    titleLabel: 'בחירת תאריך',  //Optional
+    todayLabel: 'היום',  //Optional
+    closeLabel: 'סגור',  //Optional
+    setLabel: 'בחר',  //Optional
+    setButtonType : 'button-assertive',  //Optional
+    todayButtonType : 'button-assertive',  //Optional
+    closeButtonType : 'button-assertive',  //Optional
+    inputDate: new Date(),  //Optional
+    mondayFirst: false,  //Optional
+    weekDaysList: ["א'", "ב'", "ג'", "ד'", "ה'", "ו'", "ש'"], //Optional
+    monthList: ["יאנ", "פבר", "מרץ", "אפר", "מאי", "יונ", "יול", "אוג", "ספט", "אוק", "נוב", "דצמ"], //Optional
+    templateType: 'popup', //Optional
+    showTodayButton: 'true', //Optional
+    modalHeaderColor: 'bar-positive', //Optional
+    modalFooterColor: 'bar-positive', //Optional
+    from: new Date(), //Optional
+    callback: function (val) {  //Mandatory
+      $scope.selectedDate = val;פ
+    },
+    dateFormat: 'dd-MM-yyyy', //Optional
+    closeOnSelect: false, //Optional
+  };
 
   ClientsService.getData(function (data) {
     $scope.items = data;
@@ -20,7 +42,7 @@
 
   $scope.showTenantsPopup = function(item) {
     $scope.selectedApp = item;
-    var alertPopup = $ionicPopup.alert({
+    $scope.TenantsPopup = $ionicPopup.alert({
       title: item.name,
       subTitle: "<img src=\'" + item.image +"\' />",
       templateUrl: "popupTenantsTemplate.html",
@@ -29,32 +51,41 @@
         text: 'חזור',
         type: 'button-default',
         onTap: function(e) {
-          //$scope.selected = {};
+          $scope.selectedApp = {};
         }
       }]
     });
 
     $scope.showTimePopup = function(item) {
-      var alertPopup = $ionicPopup.alert({
-        title: $scope.selectedApp.name,
-        subTitle: item.name,
+      $scope.now = Date.today().setTimeToNow();;
+      console.log($scope.now);
+      $scope.selectedTenant = item;
+      $scope.displayEffect =  ionicMaterialInk.displayEffect;
+      $scope.TimePopup = $ionicPopup.alert({
+        title: $scope.selectedApp.name + " - " + item.name,
+        subTitle: "מתי לחזור אליך?",
         templateUrl: "popupTimeTemplate.html",
         scope: $scope,
         buttons: [{
           text: 'חזור',
           type: 'button-default',
           onTap: function (e) {
-            return null;
+            $scope.selectedTenant = {};
           }
         }]
       });
     }
 
-      $scope.sendDetails = function(selectedTenant){
+      $scope.sendDetails = function(selectedTime){
+        if($scope.TimePopup){
+          $scope.TimePopup.close();
+          $scope.TenantsPopup.close();
+        }
+
       //var phone = $localStorage.myAppData.phone;
-      console.log(selectedTenant);
+      console.log(selectedTime);
       var phone = "0525675119";
-      var xmlData = "<XmlTemplate><WebServiceURL>https://sec-phoneplus.com/DistributionWebService/DialerDistribution.asmx/ImportFromXml</WebServiceURL><TenantID>"+selectedTenant.tenantID+"</TenantID><TenantName>"+selectedTenant.name+"</TenantName><ApplicationID>"+$scope.selectedApp.appID+"</ApplicationID><ApplicationVersion>1</ApplicationVersion><ApplicationName>"+$scope.selectedApp.name+"</ApplicationName><SecID>"+selectedTenant.secID+"</SecID><DialerRequestTime>"+new Date().toString('yyyy-MM-dd HH:mm:ss')+"</DialerRequestTime><PhoneNum>" + phone + "</PhoneNum><PhoneNum2></PhoneNum2><PhoneNum3></PhoneNum3><AgentExtNumber></AgentExtNumber><Info1></Info1><Info2></Info2><Info3></Info3><Info4></Info4><Info5></Info5></XmlTemplate>";
+      var xmlData = "<XmlTemplate><WebServiceURL>https://sec-phoneplus.com/DistributionWebService/DialerDistribution.asmx/ImportFromXml</WebServiceURL><TenantID>"+$scope.selectedTenant.tenantID+"</TenantID><TenantName>"+$scope.selectedTenant.name+"</TenantName><ApplicationID>"+$scope.selectedApp.appID+"</ApplicationID><ApplicationVersion>1</ApplicationVersion><ApplicationName>"+$scope.selectedApp.name+"</ApplicationName><SecID>"+$scope.selectedTenant.secID+"</SecID><DialerRequestTime>"+selectedTime.toString('yyyy-MM-dd HH:mm:ss')+"</DialerRequestTime><PhoneNum>" + phone + "</PhoneNum><PhoneNum2></PhoneNum2><PhoneNum3></PhoneNum3><AgentExtNumber></AgentExtNumber><Info1></Info1><Info2></Info2><Info3></Info3><Info4></Info4><Info5></Info5></XmlTemplate>";
       console.log(xmlData);
 
       $.ajax({
@@ -65,9 +96,9 @@
           alert('Success');
         },
         error: function (jqXHR, ajaxOptions, thrownError) {
-          alert('שגיאה:\r\n' + jqXHR.responseText);
-          alert('2: '+ ajaxOptions);
-          alert('3: '+ thrownError);
+          //alert('שגיאה:\r\n' + jqXHR.responseText);
+          //alert('2: '+ ajaxOptions);
+          //alert('3: '+ thrownError);
 
           console.log(jqXHR);
           console.log(ajaxOptions);
