7db48a215633f7c0b7466a0c5a69b6d771b54a7a
gadi gomez <gadi gomez>
2016-03-19 22:28:00 +0200

    added pull to refresh and white list

--- www/js/controllers/IntroCtrl.js
+++ www/js/controllers/IntroCtrl.js
@@ -1,8 +1,8 @@
-app.controller('IntroCtrl', function($scope, $state,$window, $localStorage) {
+app.controller('IntroCtrl', function($scope, $state,$window, $localStorage, $ionicPopup) {
   // here we store wizard data
   $scope.button = "שלח SMS לאימות";
   $scope.step2 = {};
-
+$scope.message = "";
   $scope.regex = "^0[0-9]{9}$";
 
   function persistWizardData() {
@@ -18,7 +18,45 @@ app.controller('IntroCtrl', function($scope, $state,$window, $localStorage) {
   $scope.sendSMS = function(number) {
     if(!$scope.showVerify)
     {
+
+      $scope.sentCode = Math.floor(1000 + Math.random() * 9000);
+
+      console.log($scope.sentCode);
+
       // send verification
+      var xmlData = ﻿"<XmlTemplate><WebServiceURL>https://sec-phoneplus.com/DistributionWebService/MessageDistribution.asmx/ImportFromXml</WebServiceURL><TenantID>1884</TenantID><TenantName>סמראט פון משלוח סמס מאשר</TenantName><ApplicationID>4479</ApplicationID><ApplicationVersion>1</ApplicationVersion><ApplicationName>משלוח סמס מאשר</ApplicationName><SecID>ad663fb8-096f-4944-9b0b-46f9248f5c5f</SecID><DistributionRequestTime>"+ new Date().toString('yyyy-MM-dd HH:mm:ss')  +"</DistributionRequestTime><OutboundReasonID>4</OutboundReasonID><AgentExtNumber></AgentExtNumber><TargetTelNumber1>"+number+"</TargetTelNumber1><TargetTelNumber2></TargetTelNumber2><TargetTelNumber3></TargetTelNumber3><PPWSinfo1></PPWSinfo1><PPWSinfo2></PPWSinfo2><PPWSinfo3></PPWSinfo3><PPWSinfo4></PPWSinfo4><PPWSinfo5></PPWSinfo5><PPWStext1 Caption=\"Free SMS\">"+$scope.sentCode+"</PPWStext1></XmlTemplate>";
+
+      console.log(xmlData);
+
+      $.ajax({
+        type: "POST",
+        url: 'https://sec-phoneplus.com/DistributionWebService/MessageDistribution.asmx/ImportFromXml',
+        data: xmlData,
+        success: function (jqXHR) {
+          console.log('all good');
+           $ionicPopup.show({
+            title: 'הבקשה התקבלה',
+            template: 'נחזור אליך בהקדם',
+            buttons: [
+              { text: 'אישור' }
+            ]
+          });
+        },
+        error: function (jqXHR, ajaxOptions, thrownError) {
+           $ionicPopup.show({
+            title: 'הבקשה התקבלה',
+            template: jqXHR,
+            buttons: [
+              { text: 'אישור' }
+            ]
+          });
+          console.log(jqXHR);
+          console.log(ajaxOptions);
+          console.log(thrownError);
+        }
+      });
+
+
       $scope.showVerify = true;
       $scope.button = "ערוך מס' טלפון";
     }
@@ -28,11 +66,14 @@ app.controller('IntroCtrl', function($scope, $state,$window, $localStorage) {
     }
   }
 
+      
+
   $scope.verifyNumber = function(code) {
     // verify code sent
 
-    if (true)
+    if (code === String($scope.sentCode))
     {
+
       $scope.start();
     }
   }
